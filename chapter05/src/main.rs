ea873b6e4c6392073f73f9ee0ce95c5463b87752
Eugene <dev@eepo.de>
2026-09-01 07:19:51 +0800

    update

--- chapter05/src/main.rs
+++ chapter05/src/main.rs
@@ -1,3 +1,19 @@
+// Using Structs to Structure Related Data
+//
+//
+
+struct User {
+    active : bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        active : true,
+        username : String::from("somename123"),
+        email : String::from("someemail@123.com"),
+        sign_in_count : 1,
+    };
 }
